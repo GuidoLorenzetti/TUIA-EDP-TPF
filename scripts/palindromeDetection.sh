a1dd9d7674df5c4124363927aa335975fd608129
Sol/Bri <>
2022-07-27 20:17:20 +0000

    commit sol

--- scripts/palindromeDetection.sh
+++ scripts/palindromeDetection.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 
+if [ $# -ne 1 ]
+then 
+     echo "Ingrese un argumento"
+else
+     if [ $1 == $(echo $1 | rev) ]
+     then
+          echo "Es un palindromo"
+     else
+          echo "No es un palindromo"
+     fi
+fi
