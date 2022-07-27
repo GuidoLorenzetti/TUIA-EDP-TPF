609299031293f130c47429ac364adadb5873340f
Sol/Bri <94907742+SolKidonakis@users.noreply.github.com>
2022-07-27 22:24:46 +0000

    Actualizacion allUperCase.sh

--- scripts/caseConverter.sh
+++ scripts/caseConverter.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+
+variable=$(<config.txt)
+
+
+minusculas=$(echo $variable | tr '[:upper:]' '[:lower:]' && echo $variable | tr '[:lower:]' '[:upper:]')
+
+
+echo "$variable" | tr '[:upper:]' '[:lower:]'
+
+echo "$variable" | tr '[:lower:]' '[:upper:]'
--- scripts/statsSentences.sh
+++ scripts/statsSentences.sh
@@ -1,1 +1,3 @@
 #!/bin/bash
+
+esta trabajando sol
