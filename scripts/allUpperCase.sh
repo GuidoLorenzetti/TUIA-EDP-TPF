456cc9560ae4913ca6d9e8326ecd39e7e2ffbfed
SolKidonakis <94907742+SolKidonakis@users.noreply.github.com>
2022-07-28 21:09:54 +0000

    Actualizacion 3

--- scripts/allUpperCase.sh
+++ scripts/allUpperCase.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-grep -H -oP '\b[A-Z0-9_]*[A-Z]+[A-Z0-9_]*\b' *
+#grep -H -oP '\b[A-Z0-9_]*[A-Z]+[A-Z0-9_]*\b' *
 sol
+
+#cat testfile2 | tr -d [:upper:]
+
+grep -o \<[[:upper:]][[:upper:]]*\> textfile.txt
