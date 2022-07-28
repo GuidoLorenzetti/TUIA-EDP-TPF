0c2bb739cb6494143455a4e361b52204573a53d3
SolKidonakis <94907742+SolKidonakis@users.noreply.github.com>
2022-07-28 20:51:47 +0000

    Actualizacion sol

--- scripts/findNames.sh
+++ scripts/findNames.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-sol
+#sol
 
 
-$find / -name ˈ[A-Z]*ˈ 
+#$find / -name ˈ[A-Z]*ˈ 
 
-grep -E '^[[:upper:]
+#grep -E '^[[:upper:]' file.txt
+
+#grep -oP "\w*[A-Z]+\w*" yourfile.txt
+
+#grep -o '\<[A-Z][a-z]*\>' file.txt
+
+#grep -P '(^[\s]+[A-Z][a-z]+)|(^[A-Z][a-z]+)'
+
+grep '\b[A-Z][a-z].*?\b' file.txt
