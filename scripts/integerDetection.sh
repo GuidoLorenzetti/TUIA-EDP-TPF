0978e258ecd6b57e9549dd3a56d15c3b44af27b1
Guido Lorenzetti <102630216+GuidoLorenzetti@users.noreply.github.com>
2022-07-30 22:43:41 +0000

    Actualización Integer Detection

--- scripts/integerDetection.sh
+++ scripts/integerDetection.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+texto=$(</home/runner/TUIA-EDP-TPF/Textos/texto.txt)
 
+rm lista1.txt
+for i in $texto:
+do
+  if [[ "$i" =~ ^[0-9]+$ ]];
+  then 
+    echo $i >> lista1.txt
+  fi
+done
 
-hola 
+echo La lista ordenada de enteros es:
+cat lista1.txt | sort -n -u
--- scripts/statsSentences.sh
+++ scripts/statsSentences.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-
+s="Here is a sample matching line
+There are none here"
+grep -i '\<[^[:digit:][:punct:][:space:]aeiou]*[aeiou][^[:digit:][:punct:][:space:]aeiou]*\>'
