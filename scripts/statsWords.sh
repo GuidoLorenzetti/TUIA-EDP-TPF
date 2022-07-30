2123c983683a1d00758ac30d3e664374df77139a
Guido Lorenzetti <102630216+GuidoLorenzetti@users.noreply.github.com>
2022-07-30 18:34:45 +0000

    Actualización statsWords.sh

--- scripts/statsWords.sh
+++ scripts/statsWords.sh
@@ -1,9 +1,35 @@
 #!/bin/bash/
-#Esta trabajando Guido
+
 texto=$(</home/runner/TUIA-EDP-TPF/README.txt)
 
-
+cont=0
+long=0
 for i in $texto;
 do
-  echo $i
+  if [ $cont -eq 0 ];
+  then
+    min=${#i}
+    corta=$i
+    max=${#i}
+    larga=$i
+  else
+    if [ ${#i} -lt $min ];
+    then
+      min=${#i}
+      corta=$i
+    fi
+    if [ ${#i} -gt $max ];
+    then
+      max=${#i}
+      larga=$i
+    fi
+  fi
+  let long=long+${#i}
+  let cont=cont+1
 done
+
+promedio=$(($long/$cont))
+
+echo La palabra mas corta es $corta
+echo La palabra mas larga es $larga
+echo El promedio de longitud es $promedio
