5fdfd9c466d6e1d6bfe80d07adea7e31b1f60304
SolKidonakis <94907742+SolKidonakis@users.noreply.github.com>
2022-08-01 20:06:31 +0000

    Actualizacion

--- scripts/caseConverter.sh
+++ scripts/caseConverter.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
 
 
-#variable=$(<config.txt)
-
-
-#minusculas=$(echo $variable | tr '[:upper:]' '[:lower:]' && echo $variable | tr '[:lower:]' '[:upper:]')
-#file=$(file.txt)
 
 file=$(</home/runner/TUIA-EDP-TPF/Textos/texto.txt)
 
-for word in $file:
-do
-  echo $word | tr '[:upper:]' '[:lower:]' 
-done 
 
 for word in $file:
-do 
-  echo $word | tr '[:lower:]' '[:upper:]'
-done
+do
+  echo $word | tr 'a-zA-Z' 'A-Za-z' | sed  's/é/É/g' | sed 's/í/Í/g' | sed  's/ó/Ó/g' | sed  's/ú/Ú/g' | sed  's/á/Á/g' 
+done 
+
+#| tr 'a-zA-Z' 'A-Za-z'  | sed  's/É/é/g' | sed 's/Í/í/g' | sed  's/Ó/ó/g' | sed  's/Ú/ú/g' | sed  's/Á/á/g'
--- scripts/findNames.sh
+++ scripts/findNames.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
 
 
-
-
-#$find / -name ˈ[A-Z]*ˈ 
-
-#grep -E '^[[:upper:]' file.txt
-
-#grep -oP "\w*[A-Z]+\w*" yourfile.txt
-
-#grep -o '\<[A-Z][a-z]*\>' file.txt
-
-#grep -P '(^[\s]+[A-Z][a-z]+)|(^[A-Z][a-z]+)'
-
 texto=$(</home/runner/TUIA-EDP-TPF/Textos/texto.txt)
 
 
