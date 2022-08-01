41157999fcbb29d5ccd7a6f9e187b95a98b88fe9
SolKidonakis <94907742+SolKidonakis@users.noreply.github.com>
2022-08-01 14:14:52 +0000

    Actualizacion

--- scripts/palindromeDetection.sh
+++ scripts/palindromeDetection.sh
@@ -2,14 +2,13 @@
 
 
 
-
-function search
+function palindromo
 {
-    grep -oiE '[a-z]{3,}' "$@" | tr '[:upper:]' '[:lower:]' | sort | while read -r word; do
+    grep -oiE '[a-z]{3,}' "$@" | tr '[:upper:]' '[:lower:]' | sed 's/é/e/g' | sed 's/á/a/g'  | sed 's/í/i/g' | sed 's/ó/o/g'  | sed 's/ú/u/g' | sed 's/É/E/g'  | sed 's/Á/A/g'  | sed 's/Í/I/g'  | sed 's/Ó/O/g' | sed 's/Ú/Ú/g'  | while read -r word; do
         [[ $word == $(rev <<< "$word") ]] && echo "$word"
     done
 }
 
 echo Palabras palíndromo:
-search $"/home/runner/TUIA-EDP-TPF/Textos/texto.txt"
+palindromo $"/home/runner/TUIA-EDP-TPF/Textos/texto.txt"
 
