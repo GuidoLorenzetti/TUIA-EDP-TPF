56b9748e34af2fee04ada4207f54385b202161c6
SolKidonakis <94907742+SolKidonakis@users.noreply.github.com>
2022-08-02 19:32:04 +0000

    Actualizacion

--- scripts/caseConverter.sh
+++ scripts/caseConverter.sh
@@ -7,7 +7,5 @@ file=$(</home/runner/TUIA-EDP-TPF/Textos/texto.txt)
 
 for word in $file:
 do
-  echo $word | tr 'a-zA-Z' 'A-Za-z' | sed  's/é/É/g' | sed 's/í/Í/g' | sed  's/ó/Ó/g' | sed  's/ú/Ú/g' | sed  's/á/Á/g' 
-done 
-
-#| tr 'a-zA-Z' 'A-Za-z'  | sed  's/É/é/g' | sed 's/Í/í/g' | sed  's/Ó/ó/g' | sed  's/Ú/ú/g' | sed  's/Á/á/g'
+  echo $word | tr 'a-zA-Z' 'A-Za-z' | sed 's/é/E/g' | sed 's/á/A/g'  | sed 's/í/I/g' | sed 's/ó/O/g'  | sed 's/ú/U/g' | sed 's/É/e/g'  | sed 's/Á/a/g'  | sed 's/Í/i/g'  | sed 's/Ó/o/g' | sed 's/Ú/u/g' 
+done
