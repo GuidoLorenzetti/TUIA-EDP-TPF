139efc1f94df59fd0a369422ecd3f0d5d2fe3ded
SolKidonakis <94907742+SolKidonakis@users.noreply.github.com>
2022-08-06 22:10:25 +0000

    Actualizacion

---
+++ scripts/substringReplace.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+texto=$(<$1)
+
+echo > lista1.txt
+
+read -p "Inserte primer cadena: " cad1
+read -p "Inserte segunda cadena: " cad2
+
+for i in $texto
+do
+ echo $i | sed 's/é/e/g' | sed 's/á/a/g'  | sed 's/í/i/g' | sed 's/ó/o/g'  | sed 's/ú/u/g' | sed 's/É/E/g'  | sed 's/Á/A/g'  | sed 's/Í/I/g'  | sed 's/Ó/O/g' | sed 's/Ú/U/g' >> lista1.txt
+  
+done
+
+lista=$(</home/runner/TUIA-EDP-TPF/scripts/lista1.txt)
+
+for x in $lista
+do
+  if [[ "$x" == *"$cad1"* ]]
+  then
+    echo "${x/"$cad1"/"$cad2"}" 
+  fi
+done
