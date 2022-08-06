b9e8a995ef1690b80495f3ee7d0a683ff8dec9cc
Guido Lorenzetti <102630216+GuidoLorenzetti@users.noreply.github.com>
2022-08-06 18:47:11 +0000

    Actualización CaseConverter.sh

--- scripts/caseConverter.sh
+++ scripts/caseConverter.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-texto=$(<$1)
+texto=$1
 
-for word in $texto:
+while IFS= read -r line
 do
-  echo $word | tr 'a-zA-Z' 'A-Za-z' | sed 's/é/E/g' | sed 's/á/A/g'  | sed 's/í/I/g' | sed 's/ó/O/g'  | sed 's/ú/U/g' | sed 's/É/e/g'  | sed 's/Á/a/g'  | sed 's/Í/i/g'  | sed 's/Ó/o/g' | sed 's/Ú/u/g' 
-done
+  echo $line | tr 'a-zA-Z' 'A-Za-z' | sed 's/é/E/g' | sed 's/á/A/g'  | sed 's/í/I/g' | sed 's/ó/O/g'  | sed 's/ú/U/g' | sed 's/É/e/g'  | sed 's/Á/a/g'  | sed 's/Í/i/g'  | sed 's/Ó/o/g' | sed 's/Ú/u/g' 
+done < $texto
