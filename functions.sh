1b37bc46802676a00f99a57a12b8510caf729a68
Guido Lorenzetti <102630216+GuidoLorenzetti@users.noreply.github.com>
2022-08-06 18:51:14 +0000

    Actualización palindromeDetection.sh

--- functions.sh
+++ functions.sh
@@ -78,3 +78,10 @@ function texto {
   read txt
   dir=/home/runner/TUIA-EDP-TPF/Textos/$txt
 }
+
+function palindromo
+{
+    grep -oiE '[a-z]{3,}' "$@" | tr '[:upper:]' '[:lower:]' | sed 's/é/e/g' | sed 's/á/a/g'  | sed 's/í/i/g' | sed 's/ó/o/g'  | sed 's/ú/u/g' | sed 's/É/E/g'  | sed 's/Á/A/g'  | sed 's/Í/I/g'  | sed 's/Ó/O/g' | sed 's/Ú/Ú/g'  | while read -r word; do
+        [[ $word == $(rev <<< "$word") ]] && echo "$word"
+    done
+}
--- scripts/palindromeDetection.sh
+++ scripts/palindromeDetection.sh
@@ -2,13 +2,7 @@
 
 texto=$1
 
-function palindromo
-{
-    grep -oiE '[a-z]{3,}' "$@" | tr '[:upper:]' '[:lower:]' | sed 's/é/e/g' | sed 's/á/a/g'  | sed 's/í/i/g' | sed 's/ó/o/g'  | sed 's/ú/u/g' | sed 's/É/E/g'  | sed 's/Á/A/g'  | sed 's/Í/I/g'  | sed 's/Ó/O/g' | sed 's/Ú/Ú/g'  | while read -r word; do
-        [[ $word == $(rev <<< "$word") ]] && echo "$word"
-    done
-}
+source ../functions.sh
 
 echo Palabras palíndromo:
 palindromo $texto
-
