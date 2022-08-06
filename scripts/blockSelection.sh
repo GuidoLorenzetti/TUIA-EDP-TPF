be437f3ab656fa31f14d0d2ff84b0db9a8324c9d
Mica Pozzo <107519381+MicaPozzo@users.noreply.github.com>
2022-08-06 22:07:10 +0000

    Actualizacion blockSelection.sh

--- scripts/blockSelection.sh
+++ scripts/blockSelection.sh
@@ -1,4 +1,38 @@
 #!/bin/bash
 
-texto=$(<$1)
+texto=$1
+cat /dev/null > lista1.txt
+while IFS= read -r line
+do
+  if [ ${#line} -gt 0 ]
+  then
+    echo $line >> lista1.txt
+  fi
+done < $texto
 
+read -p "Ingrese P para parrafos, O para oraciones: " opc
+
+if [ "$opc" = "O" ]
+then
+  echo entre O
+  cat lista1.txt | sed 's/[.!?]  */&\n/g' > lista.txt
+fi
+if [ "$opc" = "P" ]
+then
+  echo entre P
+  cat lista1.txt > lista.txt
+fi
+
+read -p "Ingrese el numero de linea: " opc
+
+cont=1
+lista=/home/runner/TUIA-EDP-TPF/scripts/lista.txt
+echo La linea elegida es:
+while IFS= read -r line
+do
+  if [ $cont -eq $opc ]
+  then
+    echo $line
+  fi
+  let cont=$cont+1
+done < $lista
