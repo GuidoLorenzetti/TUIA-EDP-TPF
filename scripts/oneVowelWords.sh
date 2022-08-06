def962bded23fd5abc4b656ea7f0e2b0de1f715d
Guido Lorenzetti <102630216+GuidoLorenzetti@users.noreply.github.com>
2022-08-06 19:06:22 +0000

    Actualización oneVowelWords.sh

--- scripts/oneVowelWords.sh
+++ scripts/oneVowelWords.sh
@@ -13,11 +13,11 @@ do
   fi
 done
 
-cat lista1.txt | grep a | grep -v e | grep -v i | grep -v o | grep -v u | grep -v é | grep -v í | grep -v ó | grep -v ú | grep -v E | grep -v I | grep -v O | grep -v U >> lista.txt
-cat lista1.txt | grep e | grep -v a | grep -v i | grep -v o | grep -v u | grep -v á | grep -v í | grep -v ó | grep -v ú | grep -v A | grep -v I | grep -v O | grep -v U >> lista.txt
-cat lista1.txt | grep i | grep -v e | grep -v a | grep -v o | grep -v u | grep -v é | grep -v á | grep -v ó | grep -v ú | grep -v E | grep -v A | grep -v O | grep -v U >> lista.txt
-cat lista1.txt | grep o | grep -v a | grep -v i | grep -v e | grep -v u | grep -v á | grep -v í | grep -v é | grep -v ú | grep -v A | grep -v I | grep -v E | grep -v U >> lista.txt
-cat lista1.txt | grep u | grep -v a | grep -v i | grep -v o | grep -v e | grep -v á | grep -v í | grep -v ó | grep -v é | grep -v A | grep -v I | grep -v O | grep -v U >> lista.txt
+cat lista1.txt | grep -i a | grep -v e | grep -v i | grep -v o | grep -v u | grep -v é | grep -v í | grep -v ó | grep -v ú | grep -v E | grep -v I | grep -v O | grep -v U >> lista.txt
+cat lista1.txt | grep -i e | grep -v a | grep -v i | grep -v o | grep -v u | grep -v á | grep -v í | grep -v ó | grep -v ú | grep -v A | grep -v I | grep -v O | grep -v U >> lista.txt
+cat lista1.txt | grep -i i | grep -v e | grep -v a | grep -v o | grep -v u | grep -v é | grep -v á | grep -v ó | grep -v ú | grep -v E | grep -v A | grep -v O | grep -v U >> lista.txt
+cat lista1.txt | grep -i o | grep -v a | grep -v i | grep -v e | grep -v u | grep -v á | grep -v í | grep -v é | grep -v ú | grep -v A | grep -v I | grep -v E | grep -v U >> lista.txt
+cat lista1.txt | grep -i u | grep -v a | grep -v i | grep -v o | grep -v e | grep -v á | grep -v í | grep -v ó | grep -v é | grep -v A | grep -v I | grep -v O | grep -v E >> lista.txt
 
 echo Las palabras monovocalicas son:
 cat lista.txt | sort -n | uniq 
